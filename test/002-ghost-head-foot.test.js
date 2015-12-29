cfe7066601a95ba2aa7498e620adefe6c83cb019
Hannah Wolfe <erisds@gmail.com>
2015-12-29 17:17:00 +0000

    Refactor to read hbs before checks

--- test/002-ghost-head-foot.test.js
+++ test/002-ghost-head-foot.test.js
@@ -1,6 +1,6 @@
 /*globals describe, it */
 var should = require('should'),
-    themePath = require('./utils').themePath,
+    utils = require('./utils'),
     thisCheck = require('../lib/checks/002-ghost-head-foot');
 
 /**
@@ -14,7 +14,7 @@ var should = require('should'),
 
 describe('Ghost head & foot', function () {
     it('should show errors for missing ghost head & foot helpers when no .hbs files are present(theme example a)', function (done) {
-        thisCheck.check(themePath('example-a')).then(function (output) {
+        utils.testCheck(thisCheck, 'example-a').then(function (output) {
             output.errors.should.be.an.Array().with.lengthOf(2);
             output.errors[0].should.match(/helper not present/);
             output.errors[0].should.match(/ghost_head/);
@@ -27,7 +27,7 @@ describe('Ghost head & foot', function () {
     });
 
     it('should show errors for missing ghost head & foot helpers when they are not in any .hbs file (theme example d)', function (done) {
-        thisCheck.check(themePath('example-d')).then(function (output) {
+        utils.testCheck(thisCheck, 'example-d').then(function (output) {
             output.errors.should.be.an.Array().with.lengthOf(2);
             output.errors[0].should.match(/helper not present/);
             output.errors[0].should.match(/ghost_head/);
@@ -40,7 +40,7 @@ describe('Ghost head & foot', function () {
     });
 
     it('should output nothing when ghost head & foot helpers are present (theme example e)', function (done) {
-        thisCheck.check(themePath('example-e')).then(function (output) {
+        utils.testCheck(thisCheck, 'example-e').then(function (output) {
             output.errors.should.be.empty();
             output.warnings.should.be.empty();
             output.recommendations.should.be.empty();
