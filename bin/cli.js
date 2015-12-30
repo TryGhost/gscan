99a4c5ddfa8a1c605e11be77f96d467866de2fa4
Hannah Wolfe <erisds@gmail.com>
2015-12-30 23:12:55 +0000

    Improve CLI version

--- bin/cli.js
+++ bin/cli.js
@@ -2,6 +2,7 @@
 
 var pkgJson = require('../package.json'),
     program = require('commander'),
+    _       = require('lodash'),
     GTC     = require('../lib'),
 
     themePath = '';
@@ -17,17 +18,28 @@ program
     })
     .parse(process.argv);
 
+function outputResult(result) {
+    console.log('\nResults:');
+    if (_.isEmpty(result)) {
+        console.log('No issues detected.');
+    } else {
+        console.log(result);
+    }
+}
 
 if (!program.args.length) {
     program.help();
 } else {
     if (program.zip) {
-        GTC.checkZip(themePath).then(function (result) {
-            console.log('checkZip', result);
-        });
+        console.log('Checking zip file...');
+        GTC.checkZip(themePath).then(outputResult);
     } else {
-        GTC.check(themePath).then(function (result) {
-            console.log('check', result);
+        console.log('Checking directory...');
+        GTC.check(themePath).then(outputResult).catch(function ENOTDIRPredicate(err) {
+            return err.code === 'ENOTDIR';
+        }, function (err) {
+            console.error(err.message);
+            console.error('Did you mean to add the -z flag to read a zip file?')
         });
     }
 }
