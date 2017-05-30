4efd53000e4aefd0502f7c57bc057db6069949ba
Aileen Nowak <AileenCGN@gmail.com>
2017-05-30 15:31:36 +0700

    🙅  Checks for deprecated features/helpers
    refs #39
    
    Adds a new checker that shows errors, if deprecated helpers or features are used in the theme:
    - `{{pageUrl}}` -> replace with `{{page_url}}`
    - `{{content words="0"}}` -> replace with `{{img_url}}`
    - `{{meta_description}}` -> remove
    - `{{@blog.posts_per_page}}` -> replace with `{{@config.posts_per_page}}`
    - `{{image}}` -> replace with `{{feature_image}}`, `{{cover_image}}` or `{{profile_image}}`
    - `{{author.image}}` -> replace with `{{author.profile_image}}`
    - `{{post.image}}` -> replace with `{{post.feature_image}}`
    - `{{@blog.cover}}` -> replace with `{{@blog.cover_image}}`
    - `{{author.cover}}` -> replace with `{{author.cover_image}}`
    - `{{tag.image}}` -> replace with `{{tag.feature_image}}`
    
    Adds tests for all cases.

---
+++ test/001-deprecations.test.js
@@ -0,0 +1,151 @@
+/*globals describe, it */
+var should = require('should'),
+    path = require('path'),
+    themePath = require('./utils').themePath,
+    checker = require('../lib/checker'),
+    thisCheck = require('../lib/checks/001-deprecations'),
+    utils = require('./utils'),
+    format = require('../lib/format');
+
+describe('Deprecations', function () {
+    it('should error if {{pageUrl}} helper is used instead of {{page_url}}(theme example m)', function (done) {
+        utils.testCheck(thisCheck, 'example-m').then(function (output) {
+            output.should.be.a.ValidThemeObject();
+
+            output.results.fail.should.be.an.Object().with.keys('GS001-DEPR-PURL', 'GS001-DEPR-MD', 'GS001-DEPR-IMG', 'GS001-DEPR-AIMG',
+                                                                'GS001-DEPR-PIMG', 'GS001-DEPR-PPP', 'GS001-DEPR-C0H', 'GS001-DEPR-PIMG',
+                                                                'GS001-DEPR-BC', 'GS001-DEPR-AC', 'GS001-DEPR-TIMG');
+
+            output.results.fail['GS001-DEPR-PURL'].should.be.a.ValidFailObject();
+
+            done();
+        }).catch(done);
+    });
+    it('should error if {{meta_description}} helper is used (theme example m)', function (done) {
+        utils.testCheck(thisCheck, 'example-m').then(function (output) {
+            output.should.be.a.ValidThemeObject();
+
+            output.results.fail.should.be.an.Object().with.keys('GS001-DEPR-PURL', 'GS001-DEPR-MD', 'GS001-DEPR-IMG', 'GS001-DEPR-AIMG',
+                                                                'GS001-DEPR-PIMG', 'GS001-DEPR-PPP', 'GS001-DEPR-C0H', 'GS001-DEPR-PIMG',
+                                                                'GS001-DEPR-BC', 'GS001-DEPR-AC', 'GS001-DEPR-TIMG');
+
+            output.results.fail['GS001-DEPR-MD'].should.be.a.ValidFailObject();
+
+            done();
+        }).catch(done);
+    });
+    it('should error if {{image}} helper is used (theme example m)', function (done) {
+        utils.testCheck(thisCheck, 'example-m').then(function (output) {
+            output.should.be.a.ValidThemeObject();
+
+            output.results.fail.should.be.an.Object().with.keys('GS001-DEPR-PURL', 'GS001-DEPR-MD', 'GS001-DEPR-IMG', 'GS001-DEPR-AIMG',
+                                                                'GS001-DEPR-PIMG', 'GS001-DEPR-PPP', 'GS001-DEPR-C0H', 'GS001-DEPR-PIMG',
+                                                                'GS001-DEPR-BC', 'GS001-DEPR-AC', 'GS001-DEPR-TIMG');
+
+            output.results.fail['GS001-DEPR-IMG'].should.be.a.ValidFailObject();
+
+            done();
+        }).catch(done);
+    });
+    it('should error if {{author.image}} helper is used (theme example m)', function (done) {
+        utils.testCheck(thisCheck, 'example-m').then(function (output) {
+            output.should.be.a.ValidThemeObject();
+
+            output.results.fail.should.be.an.Object().with.keys('GS001-DEPR-PURL', 'GS001-DEPR-MD', 'GS001-DEPR-IMG', 'GS001-DEPR-AIMG',
+                                                                'GS001-DEPR-PIMG', 'GS001-DEPR-PPP', 'GS001-DEPR-C0H', 'GS001-DEPR-PIMG',
+                                                                'GS001-DEPR-BC', 'GS001-DEPR-AC', 'GS001-DEPR-TIMG');
+
+            output.results.fail['GS001-DEPR-AIMG'].should.be.a.ValidFailObject();
+
+            done();
+        }).catch(done);
+    });
+    it('should error if {{post.image}} helper is used (theme example m)', function (done) {
+        utils.testCheck(thisCheck, 'example-m').then(function (output) {
+            output.should.be.a.ValidThemeObject();
+
+            output.results.fail.should.be.an.Object().with.keys('GS001-DEPR-PURL', 'GS001-DEPR-MD', 'GS001-DEPR-IMG', 'GS001-DEPR-AIMG',
+                                                                'GS001-DEPR-PIMG', 'GS001-DEPR-PPP', 'GS001-DEPR-C0H', 'GS001-DEPR-PIMG',
+                                                                'GS001-DEPR-BC', 'GS001-DEPR-AC', 'GS001-DEPR-TIMG');
+
+            output.results.fail['GS001-DEPR-PIMG'].should.be.a.ValidFailObject();
+
+            done();
+        }).catch(done);
+    });
+    it('should error if {{@blog.cover}} helper is used (theme example m)', function (done) {
+        utils.testCheck(thisCheck, 'example-m').then(function (output) {
+            output.should.be.a.ValidThemeObject();
+
+            output.results.fail.should.be.an.Object().with.keys('GS001-DEPR-PURL', 'GS001-DEPR-MD', 'GS001-DEPR-IMG', 'GS001-DEPR-AIMG',
+                                                                'GS001-DEPR-PIMG', 'GS001-DEPR-PPP', 'GS001-DEPR-C0H', 'GS001-DEPR-PIMG',
+                                                                'GS001-DEPR-BC', 'GS001-DEPR-AC', 'GS001-DEPR-TIMG');
+
+            output.results.fail['GS001-DEPR-BC'].should.be.a.ValidFailObject();
+
+            done();
+        }).catch(done);
+    });
+    it('should error if {{author.cover}} helper is used (theme example m)', function (done) {
+        utils.testCheck(thisCheck, 'example-m').then(function (output) {
+            output.should.be.a.ValidThemeObject();
+
+            output.results.fail.should.be.an.Object().with.keys('GS001-DEPR-PURL', 'GS001-DEPR-MD', 'GS001-DEPR-IMG', 'GS001-DEPR-AIMG',
+                                                                'GS001-DEPR-PIMG', 'GS001-DEPR-PPP', 'GS001-DEPR-C0H', 'GS001-DEPR-PIMG',
+                                                                'GS001-DEPR-BC', 'GS001-DEPR-AC', 'GS001-DEPR-TIMG');
+
+            output.results.fail['GS001-DEPR-AC'].should.be.a.ValidFailObject();
+
+            done();
+        }).catch(done);
+    });
+    it('should error if {{tag.image}} helper is used (theme example m)', function (done) {
+        utils.testCheck(thisCheck, 'example-m').then(function (output) {
+            output.should.be.a.ValidThemeObject();
+
+            output.results.fail.should.be.an.Object().with.keys('GS001-DEPR-PURL', 'GS001-DEPR-MD', 'GS001-DEPR-IMG', 'GS001-DEPR-AIMG',
+                                                                'GS001-DEPR-PIMG', 'GS001-DEPR-PPP', 'GS001-DEPR-C0H', 'GS001-DEPR-PIMG',
+                                                                'GS001-DEPR-BC', 'GS001-DEPR-AC', 'GS001-DEPR-TIMG');
+
+            output.results.fail['GS001-DEPR-TIMG'].should.be.a.ValidFailObject();
+
+            done();
+        }).catch(done);
+    });
+    it('should error if {{@blog.posts_per_page}} helper is used (theme example m)', function (done) {
+        utils.testCheck(thisCheck, 'example-m').then(function (output) {
+            output.should.be.a.ValidThemeObject();
+
+            output.results.fail.should.be.an.Object().with.keys('GS001-DEPR-PURL', 'GS001-DEPR-MD', 'GS001-DEPR-IMG', 'GS001-DEPR-AIMG',
+                                                                'GS001-DEPR-PIMG', 'GS001-DEPR-PPP', 'GS001-DEPR-C0H', 'GS001-DEPR-PIMG',
+                                                                'GS001-DEPR-BC', 'GS001-DEPR-AC', 'GS001-DEPR-TIMG');
+
+            output.results.fail['GS001-DEPR-PPP'].should.be.a.ValidFailObject();
+
+            done();
+        }).catch(done);
+    });
+    it('should error if {{content word="0"}} hack is used (theme example m)', function (done) {
+        utils.testCheck(thisCheck, 'example-m').then(function (output) {
+            output.should.be.a.ValidThemeObject();
+
+            output.results.fail.should.be.an.Object().with.keys('GS001-DEPR-PURL', 'GS001-DEPR-MD', 'GS001-DEPR-IMG', 'GS001-DEPR-AIMG',
+                                                                'GS001-DEPR-PIMG', 'GS001-DEPR-PPP', 'GS001-DEPR-C0H', 'GS001-DEPR-PIMG',
+                                                                'GS001-DEPR-BC', 'GS001-DEPR-AC', 'GS001-DEPR-TIMG');
+
+            output.results.fail['GS001-DEPR-C0H'].should.be.a.ValidFailObject();
+
+            done();
+        }).catch(done);
+    });
+    it('[SUCCESS] should show no error if no deprecated helpers used (theme example n)', function (done) {
+        utils.testCheck(thisCheck, 'example-n').then(function (output) {
+            output.should.be.a.ValidThemeObject();
+
+            output.results.fail.should.be.an.Object().which.is.empty();
+            output.results.pass.should.be.an.Array().with.lengthOf(40);
+
+            done();
+        }).catch(done);
+    });
+});
