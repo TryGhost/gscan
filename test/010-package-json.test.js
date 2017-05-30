e29add30e3cc8ebee675d04f1ea1798417707b96
kirrg001 <katharina.irrgang@googlemail.com>
2017-05-31 00:39:15 +0200

    😝  extend package.json validation
    refs #40
    
    - removed package-json-validator dependency, because it can't provide what we need
    - add semver/validator to validate the package.json on our own (be relaxed with dependencie pinning - so Ghost won't install the dependencies twice)

--- test/010-package-json.test.js
+++ test/010-package-json.test.js
@@ -3,83 +3,88 @@ var should = require('should'),
     utils = require('./utils'),
     thisCheck = require('../lib/checks/010-package-json');
 
-describe('package.json', function () {
-    it('should output error for missing package.json (theme example a)', function (done) {
-        utils.testCheck(thisCheck, 'example-a').then(function (output) {
+describe('010: package.json', function () {
+    it('should output error for missing package.json', function (done) {
+        utils.testCheck(thisCheck, 'is-empty').then(function (output) {
             output.should.be.a.ValidThemeObject();
 
-            // Should auto pass valid rule
-            output.results.pass.should.be.an.Array().with.lengthOf(1);
-            output.results.pass.should.containEql('GS010-PJ-VAL');
+            output.results.pass.should.be.an.Array().with.lengthOf(0);
 
-            // Should have resulted in a failures
-            output.results.fail.should.be.an.Object().with.keys('GS010-PJ-REQ');
+            // package.json not found, can't parse and all fields are missing + invalid
+            output.results.fail.should.be.an.Object().with.keys(
+                'GS010-PJ-REQ',
+                'GS010-PJ-PARSE',
+                'GS010-PJ-NAME-REQ',
+                'GS010-PJ-NAME-LC',
+                'GS010-PJ-NAME-HY',
+                'GS010-PJ-VERSION-SEM',
+                'GS010-PJ-VERSION-REQ',
+                'GS010-PJ-AUT-EM-VAL',
+                'GS010-PJ-AUT-EM-REQ'
+            );
+
             output.results.fail['GS010-PJ-REQ'].should.be.a.ValidFailObject();
+            done();
+        });
+    });
+
+    it('should output error for invalid package.json', function (done) {
+        utils.testCheck(thisCheck, '010-packagejson/syntax-error').then(function (output) {
+            output.should.be.a.ValidThemeObject();
+
+            output.results.pass.should.be.an.Array().with.lengthOf(0);
+
+            // can't parse package.json and all fields are missing + invalid
+            output.results.fail.should.be.an.Object().with.keys(
+                'GS010-PJ-PARSE',
+                'GS010-PJ-NAME-REQ',
+                'GS010-PJ-NAME-LC',
+                'GS010-PJ-NAME-HY',
+                'GS010-PJ-VERSION-SEM',
+                'GS010-PJ-VERSION-REQ',
+                'GS010-PJ-AUT-EM-VAL',
+                'GS010-PJ-AUT-EM-REQ'
+            );
+
+            // @TODO: is Invalid JSON helpful?
+            output.results.fail['GS010-PJ-PARSE'].should.be.a.ValidFailObject();
 
             done();
         });
     });
 
-    it('should output error for invalid package.json (theme example b)', function (done) {
-        utils.testCheck(thisCheck, 'example-b').then(function (output) {
-            output.should.be.a.ValidThemeObject();
-
-            // Should pass the exists rule
-            output.results.pass.should.be.an.Array().with.lengthOf(1);
-            output.results.pass.should.containEql('GS010-PJ-REQ');
-
-            output.results.fail.should.be.an.Object().with.keys('GS010-PJ-VAL');
-            output.results.fail['GS010-PJ-VAL'].should.be.a.ValidFailObject();
-            output.results.fail['GS010-PJ-VAL'].message.should.match(/Invalid JSON/);
-
+    it('valid fields', function (done) {
+        utils.testCheck(thisCheck, '010-packagejson/fields-are-valid').then(function (theme) {
+            theme.should.be.a.ValidThemeObject();
+            theme.results.fail.should.be.an.Object().which.is.empty();
             done();
         });
     });
 
-    it('should output warnings ONLY for missing author (theme example c)', function (done) {
-        utils.testCheck(thisCheck, 'example-c').then(function (output) {
-            output.should.be.a.ValidThemeObject();
+    it('invalid fields', function (done) {
+        utils.testCheck(thisCheck, '010-packagejson/fields-are-invalid').then(function (theme) {
+            theme.should.be.a.ValidThemeObject();
 
-            output.results.fail.should.be.an.Object().which.is.empty();
-
-            output.results.pass.should.be.an.Array().with.lengthOf(2);
-            output.results.pass.should.containEql('GS010-PJ-REQ');
-            output.results.pass.should.containEql('GS010-PJ-VAL');
-
+            theme.results.fail.should.be.an.Object().with.keys(
+                'GS010-PJ-NAME-LC', 'GS010-PJ-NAME-HY', 'GS010-PJ-VERSION-SEM', 'GS010-PJ-AUT-EM-VAL'
+            );
             done();
         });
     });
 
-    it('should output warning-level error for invalid version (theme example g)', function (done) {
-        utils.testCheck(thisCheck, 'example-g').then(function (output) {
-            output.should.be.a.ValidThemeObject();
+    it('missing fields', function (done) {
+        utils.testCheck(thisCheck, '010-packagejson/fields-are-missing').then(function (theme) {
+            theme.should.be.a.ValidThemeObject();
 
-            output.results.fail.should.be.an.Object().with.keys('GS010-PJ-VAL');
-            output.results.fail['GS010-PJ-VAL'].should.be.a.ValidFailObject();
-            output.results.fail['GS010-PJ-VAL'].failures.should.be.an.Array().with.lengthOf(1);
-            output.results.fail['GS010-PJ-VAL'].failures[0].should.have.keys('ref', 'message');
-
-            output.results.pass.should.be.an.Array().with.lengthOf(1);
-            output.results.pass.should.containEql('GS010-PJ-REQ');
-
-
-            done();
-        });
-    });
-
-    it('should output warning-level errors when there are multiple errors (theme example h)', function (done) {
-        utils.testCheck(thisCheck, 'example-h').then(function (output) {
-            output.should.be.a.ValidThemeObject();
-
-            output.results.fail.should.be.an.Object().with.keys('GS010-PJ-VAL');
-            output.results.fail['GS010-PJ-VAL'].should.be.a.ValidFailObject();
-            output.results.fail['GS010-PJ-VAL'].failures.should.be.an.Array().with.lengthOf(2);
-            output.results.fail['GS010-PJ-VAL'].failures[0].should.have.keys('ref', 'message');
-            output.results.fail['GS010-PJ-VAL'].failures[1].should.have.keys('ref', 'message');
-
-            output.results.pass.should.be.an.Array().with.lengthOf(1);
-            output.results.pass.should.containEql('GS010-PJ-REQ');
-
+            theme.results.fail.should.be.an.Object().with.keys(
+                'GS010-PJ-NAME-REQ',
+                'GS010-PJ-NAME-LC',
+                'GS010-PJ-NAME-HY',
+                'GS010-PJ-VERSION-SEM',
+                'GS010-PJ-VERSION-REQ',
+                'GS010-PJ-AUT-EM-VAL',
+                'GS010-PJ-AUT-EM-REQ'
+            );
 
             done();
         });
