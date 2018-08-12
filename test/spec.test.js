df489157835855383522b3db4ada484ba64bd7fe
Aileen Nowak <AileenCGN@gmail.com>
2018-08-12 13:58:25 +0800

    Added ability to remove rules from checks
    closes #135
    
    - added functionality in the latest spec to exlude helpers, templates, or rules from further checks

---
+++ test/spec.test.js
@@ -0,0 +1,58 @@
+const should = require('should'); // eslint-disable-line no-unused-vars
+const rewire = require('rewire');
+
+const specs = require('../lib/specs');
+
+describe('Specs', function () {
+    describe('getter', function () {
+        it('returns a valid ruleset', function () {
+            const v1Spec = specs.get('v1');
+            const latestSpec = specs.get('latest');
+
+            v1Spec.should.be.an.Object().with.keys(
+                'knownHelpers',
+                'templates',
+                'rules'
+            );
+
+            latestSpec.should.be.an.Object().with.keys(
+                'knownHelpers',
+                'templates',
+                'rules'
+            );
+        });
+    });
+
+    describe('v1 spec', function () {
+        let v1Spec;
+
+        it('returns v1', function () {
+            v1Spec = require('../lib/specs/v1');
+
+            v1Spec.should.be.an.Object().with.keys(
+                'knownHelpers',
+                'templates',
+                'rules'
+            );
+
+            v1Spec.knownHelpers.should.be.an.Array().with.lengthOf(46);
+            v1Spec.templates.should.be.an.Array().with.lengthOf(10);
+            v1Spec.rules.should.be.an.Object();
+            Object.keys(v1Spec.rules).length.should.eql(49);
+        });
+    });
+
+    describe('latest spec', function () {
+        it('returns merged specs', function () {
+            const latest = rewire('../lib/specs/latest');
+
+            latest.knownHelpers.should.be.an.Array().with.lengthOf(46);
+            latest.templates.should.be.an.Array().with.lengthOf(10);
+            latest.rules.should.be.an.Object();
+            Object.keys(latest.rules).length.should.eql(92);
+        });
+
+        it('returns updated rules');
+        it('can remove rules');
+    });
+});
