0ccf102d1a7eb5f05fef398c515855d668743104
Nazar Gargol <nazargargol@gmail.com>
2019-09-13 11:04:16 +0200

    Addev copies for v4 suites where test cases differ between versions

--- test/050-koenig-css-classes.test.js
+++ test/050-koenig-css-classes.test.js
@@ -175,4 +175,84 @@ describe('050 Koenig CSS classes', function () {
             }).catch(done);
         });
     });
+
+    describe('v4:', function () {
+        const options = {checkVersion: 'v4'};
+
+        it('[failure] should invalidate theme when .css file is missing', function (done) {
+            utils.testCheck(thisCheck, 'is-empty', options).then(function (output) {
+                output.should.be.a.ValidThemeObject();
+
+                output.results.pass.should.be.an.Array().which.is.empty();
+
+                output.results.fail.should.be.an.Object().with.keys('GS050-CSS-KGWW', 'GS050-CSS-KGWF', 'GS050-CSS-KGGC', 'GS050-CSS-KGGR', 'GS050-CSS-KGGI');
+
+                output.results.fail['GS050-CSS-KGWW'].should.be.a.ValidFailObject();
+                output.results.fail['GS050-CSS-KGWF'].should.be.a.ValidFailObject();
+                output.results.fail['GS050-CSS-KGGC'].should.be.a.ValidFailObject();
+                output.results.fail['GS050-CSS-KGGR'].should.be.a.ValidFailObject();
+                output.results.fail['GS050-CSS-KGGI'].should.be.a.ValidFailObject();
+
+                output.results.fail['GS050-CSS-KGWF'].failures.length.should.eql(1);
+                output.results.fail['GS050-CSS-KGWW'].failures.length.should.eql(1);
+                output.results.fail['GS050-CSS-KGGC'].failures.length.should.eql(1);
+                output.results.fail['GS050-CSS-KGGR'].failures.length.should.eql(1);
+                output.results.fail['GS050-CSS-KGGI'].failures.length.should.eql(1);
+
+                output.results.fail['GS050-CSS-KGWF'].failures[0].ref.should.eql('styles');
+                output.results.fail['GS050-CSS-KGWW'].failures[0].ref.should.eql('styles');
+                output.results.fail['GS050-CSS-KGGC'].failures[0].ref.should.eql('styles');
+                output.results.fail['GS050-CSS-KGGR'].failures[0].ref.should.eql('styles');
+                output.results.fail['GS050-CSS-KGGI'].failures[0].ref.should.eql('styles');
+
+                done();
+            }).catch(done);
+        });
+
+        it('[failure] should invalidate theme when CSS classes are missing', function (done) {
+            utils.testCheck(thisCheck, '050-koenig-css-classes/missing', options).then(function (output) {
+                output.should.be.a.ValidThemeObject();
+
+                output.results.pass.should.be.an.Array().which.is.empty();
+
+                output.results.fail.should.be.an.Object().with.keys('GS050-CSS-KGWW', 'GS050-CSS-KGWF', 'GS050-CSS-KGGC', 'GS050-CSS-KGGR', 'GS050-CSS-KGGI');
+
+                output.results.fail['GS050-CSS-KGWW'].should.be.a.ValidFailObject();
+                output.results.fail['GS050-CSS-KGWF'].should.be.a.ValidFailObject();
+                output.results.fail['GS050-CSS-KGGC'].should.be.a.ValidFailObject();
+                output.results.fail['GS050-CSS-KGGR'].should.be.a.ValidFailObject();
+                output.results.fail['GS050-CSS-KGGI'].should.be.a.ValidFailObject();
+
+                done();
+            }).catch(done);
+        });
+
+        it('[failure] should invalidate theme when three CSS classes are missing (classes are spread in hbs and css files)', function (done) {
+            utils.testCheck(thisCheck, '050-koenig-css-classes/mixed', options).then(function (output) {
+                output.should.be.a.ValidThemeObject();
+
+                output.results.pass.should.be.an.Array().with.lengthOf(2);
+
+                output.results.fail.should.be.an.Object().with.keys('GS050-CSS-KGWF', 'GS050-CSS-KGGC', 'GS050-CSS-KGGI');
+
+                output.results.fail['GS050-CSS-KGWF'].should.be.a.ValidFailObject();
+                output.results.fail['GS050-CSS-KGGC'].should.be.a.ValidFailObject();
+                output.results.fail['GS050-CSS-KGGI'].should.be.a.ValidFailObject();
+
+                done();
+            }).catch(done);
+        });
+
+        it('[success] should pass theme when CSS classes are present', function (done) {
+            utils.testCheck(thisCheck, '050-koenig-css-classes/valid', options).then(function (output) {
+                output.should.be.a.ValidThemeObject();
+
+                output.results.pass.should.be.an.Array().with.lengthOf(5);
+
+                output.results.fail.should.be.an.Object().which.is.empty();
+
+                done();
+            }).catch(done);
+        });
+    });
 });
