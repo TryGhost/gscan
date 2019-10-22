e7907239631050973893ef9304280f92e910f167
Nazar Gargol <nazargargol@gmail.com>
2019-10-22 10:53:25 +0200

    Extracted format suite out of general test

--- test/general.test.js
+++ test/general.test.js
@@ -9,7 +9,6 @@ const themePath = require('./utils').themePath;
 const readZip = require('../lib/read-zip');
 const readTheme = rewire('../lib/read-theme');
 const checker = require('../lib/checker');
-const format = require('../lib/format');
 
 const sandbox = sinon.sandbox.create();
 
@@ -317,92 +316,3 @@ describe('Read Hbs Files', function () {
     });
 });
 
-describe('format', function () {
-    it('assert sorting', function (done) {
-        checker(themePath('005-compile/invalid')).then((theme) => {
-            theme = format(theme);
-
-            theme.results.error.length.should.eql(26);
-            theme.results.error[0].fatal.should.eql(true);
-            // theme.results.error[1].fatal.should.eql(true);
-            // theme.results.error[2].fatal.should.eql(true);
-            theme.results.error[3].fatal.should.eql(false);
-            // theme.results.error[10].fatal.should.eql(false);
-
-            done();
-        }).catch(done);
-    });
-
-    it('assert sorting', function (done) {
-        checker(themePath('is-empty')).then((theme) => {
-            theme = format(theme);
-
-            theme.results.error[0].fatal.should.eql(true);
-            theme.results.error[1].fatal.should.eql(true);
-            theme.results.error[4].fatal.should.eql(false);
-            theme.results.error[10].fatal.should.eql(false);
-            theme.results.error[11].fatal.should.eql(false);
-            theme.results.error[12].fatal.should.eql(false);
-
-            done();
-        }).catch(done);
-    });
-
-    it('sort by files', function (done) {
-        checker(themePath('005-compile/invalid')).then((theme) => {
-            theme = format(theme, {sortByFiles: true});
-
-            theme.results.hasFatalErrors.should.be.true();
-
-            theme.results.recommendation.all.length.should.eql(1);
-            theme.results.recommendation.byFiles['package.json'].length.should.eql(1);
-
-            theme.results.warning.all.length.should.eql(3);
-            theme.results.warning.byFiles['default.hbs'].length.should.eql(2);
-
-            theme.results.error.all.length.should.eql(26);
-
-            // 1 rule has file references
-            theme.results.error.byFiles['author.hbs'].length.should.eql(1);
-            theme.results.error.byFiles['page.hbs'].length.should.eql(1);
-            theme.results.error.byFiles['post.hbs'].length.should.eql(1);
-            theme.results.error.byFiles['index.hbs'].length.should.eql(1);
-            theme.results.error.byFiles['package.json'].length.should.eql(9);
-
-            done();
-        }).catch(done);
-    });
-
-    it('sort by files', function (done) {
-        checker(themePath('001-deprecations/v2/invalid_all')).then((theme) => {
-            theme = format(theme, {sortByFiles: true});
-
-            theme.results.hasFatalErrors.should.be.true();
-
-            theme.results.recommendation.all.length.should.eql(2);
-            theme.results.recommendation.byFiles['package.json'].length.should.eql(1);
-
-            theme.results.error.all.length.should.eql(97);
-            theme.results.warning.all.length.should.eql(5);
-
-            theme.results.error.byFiles['assets/my.css'].length.should.eql(3);
-            theme.results.error.byFiles['default.hbs'].length.should.eql(16);
-            theme.results.error.byFiles['post.hbs'].length.should.eql(54);
-            theme.results.error.byFiles['partials/mypartial.hbs'].length.should.eql(5);
-            theme.results.error.byFiles['index.hbs'].length.should.eql(9);
-
-            done();
-        }).catch(done);
-    });
-
-    it('formats for CLI output', function () {
-        return checker(themePath('001-deprecations/v2/invalid_all')).then((theme) => {
-            theme = format(theme, {format: 'cli'});
-
-            theme.results.error[0].rule.should.equal('Replace \u001b[36m{{pageUrl}}\u001b[39m with \u001b[36m{{page_url}}\u001b[39m');
-
-            theme.results.error[0].details.should.startWith(`The helper \u001b[36m{{pageUrl}}\u001b[39m was replaced with \u001b[36m{{page_url}}\u001b[39m.\n`);
-            theme.results.error[0].details.should.endWith(`Find more information about the \u001b[36m{{page_url}}\u001b[39m helper here (https://ghost.org/docs/api/handlebars-themes/helpers/pagination/).`);
-        });
-    });
-});
