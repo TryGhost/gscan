81cc9336efaf03bbbf74c63d12be86088ff5c8ee
Thibaut Patel <thibaut.patel@gmail.com>
2021-03-12 19:23:02 +0100

    💡 Updated theme translation validity check to be fatal when failing (#375)
    no issue

--- test/070-theme-translations.test.js
+++ test/070-theme-translations.test.js
@@ -2,6 +2,9 @@ const should = require('should'); // eslint-disable-line no-unused-vars
 const utils = require('./utils');
 const thisCheck = require('../lib/checks/070-theme-translations');
 
+const checker = require('../lib/checker');
+const format = require('../lib/format');
+
 describe('070 Theme Translations', function () {
     it('should not run check for v1', function (done) {
         const options = {checkVersion: 'v1'};
@@ -28,21 +31,28 @@ describe('070 Theme Translations', function () {
     });
 
     it('should fail when a theme has invalid locales', function (done) {
-        const options = {checkVersion: 'v3'};
-        utils.testCheck(thisCheck, '070-theme-translations/invalid', options).then(function (output) {
-            output.should.be.a.ValidThemeObject();
+        const options = {checkVersion: 'canary'};
+        checker(utils.themePath('070-theme-translations/invalid'), options).then((theme) => {
+            theme.should.be.a.ValidThemeObject();
 
-            output.results.pass.should.be.an.Array().which.is.empty();
-            output.results.fail.should.be.an.Object().with.keys('GS070-VALID-TRANSLATIONS');
+            theme.results.pass.should.be.an.Array().not.with.keys('GS070-VALID-TRANSLATIONS');
+            theme.results.fail.should.be.an.Object().with.keys('GS070-VALID-TRANSLATIONS');
 
-            output.results.fail['GS070-VALID-TRANSLATIONS'].should.be.a.ValidFailObject();
-            output.results.fail['GS070-VALID-TRANSLATIONS'].failures.should.be.an.Array().with.lengthOf(2);
+            theme.results.fail['GS070-VALID-TRANSLATIONS'].should.be.a.ValidFailObject();
+            theme.results.fail['GS070-VALID-TRANSLATIONS'].failures.should.be.an.Array().with.lengthOf(2);
 
-            output.results.fail['GS070-VALID-TRANSLATIONS'].failures[0].should.have.keys('ref', 'message');
-            output.results.fail['GS070-VALID-TRANSLATIONS'].failures[0].ref.should.eql('locales/en.json');
+            theme.results.fail['GS070-VALID-TRANSLATIONS'].failures[0].should.have.keys('ref', 'message');
+            theme.results.fail['GS070-VALID-TRANSLATIONS'].failures[0].ref.should.eql('locales/en.json');
 
-            output.results.fail['GS070-VALID-TRANSLATIONS'].failures[1].should.have.keys('ref', 'message');
-            output.results.fail['GS070-VALID-TRANSLATIONS'].failures[1].ref.should.eql('locales/es.json');
+            theme.results.fail['GS070-VALID-TRANSLATIONS'].failures[1].should.have.keys('ref', 'message');
+            theme.results.fail['GS070-VALID-TRANSLATIONS'].failures[1].ref.should.eql('locales/es.json');
+
+            theme = format(theme, options);
+
+            const error = theme.results.error.find(error => error.code === 'GS070-VALID-TRANSLATIONS');
+
+            error.should.be.an.Object();
+            error.fatal.should.be.true;
 
             done();
         }).catch(done);
