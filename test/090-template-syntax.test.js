649be4354679e29f45bcd295e1c297635c06ee78
Hannah Wolfe <github.erisds@gmail.com>
2022-05-24 17:03:16 +0100

    Reworked @price helper rules (#438)
    refs: https://github.com/TryGhost/Team/issues/1648
    refs: https://github.com/TryGhost/Ghost/issues/14881
    
    - split the rule for @price into 3 separate rules:
         - two for currency which needs to be changed differently based on its context 
         - one for monthly/yearly which is much simpler
    - this is also linked to a new price helper page in the docs which explains the contexts better

--- test/090-template-syntax.test.js
+++ test/090-template-syntax.test.js
@@ -128,13 +128,67 @@ describe('090 Template syntax', function () {
             output.results.fail['GS090-NO-MEMBER-PRODUCTS-DATA-HELPER'].should.be.a.ValidFailObject();
         });
 
-        it('should fail when {{@price}} data helper is used', async function () {
-            const output = await utils.testCheck(thisCheck, '090-template-syntax/no-price-data-helper', options);
+        it('should fail when {{price currency=@price.currency}} is used', async function () {
+            const output = await utils.testCheck(thisCheck, '090-template-syntax/no-price-data-currency-global/price-helper', options);
             Object.keys(output.results.fail).should.eql([
-                'GS090-NO-PRICE-DATA-HELPER'
+                'GS090-NO-PRICE-DATA-CURRENCY-GLOBAL'
             ]);
 
-            output.results.fail['GS090-NO-PRICE-DATA-HELPER'].should.be.a.ValidFailObject();
+            output.results.fail['GS090-NO-PRICE-DATA-CURRENCY-GLOBAL'].should.be.a.ValidFailObject();
+        });
+
+        it('should fail when {{@price.currency}} is used', async function () {
+            const output = await utils.testCheck(thisCheck, '090-template-syntax/no-price-data-currency-global/global', options);
+            Object.keys(output.results.fail).should.eql([
+                'GS090-NO-PRICE-DATA-CURRENCY-GLOBAL'
+            ]);
+
+            output.results.fail['GS090-NO-PRICE-DATA-CURRENCY-GLOBAL'].should.be.a.ValidFailObject();
+        });
+
+        it('should fail when {{@price.currency}} is used inside {{#foreach @member.subscriptions}}', async function () {
+            const output = await utils.testCheck(thisCheck, '090-template-syntax/no-price-data-currency-context/member-subscriptions', options);
+            Object.keys(output.results.fail).should.eql([
+                'GS090-NO-PRICE-DATA-CURRENCY-CONTEXT'
+            ]);
+
+            output.results.fail['GS090-NO-PRICE-DATA-CURRENCY-CONTEXT'].should.be.a.ValidFailObject();
+        });
+
+        it('should fail when {{price @price.currency}} is used inside {{#foreach @member.subscriptions}}', async function () {
+            const output = await utils.testCheck(thisCheck, '090-template-syntax/no-price-data-currency-context/member-subscriptions-price-helper', options);
+            Object.keys(output.results.fail).should.eql([
+                'GS090-NO-PRICE-DATA-CURRENCY-CONTEXT'
+            ]);
+
+            output.results.fail['GS090-NO-PRICE-DATA-CURRENCY-CONTEXT'].should.be.a.ValidFailObject();
+        });
+
+        it('should fail when {{price @price.currency}} is used inside {{#foreach tiers}}', async function () {
+            const output = await utils.testCheck(thisCheck, '090-template-syntax/no-price-data-currency-context/tiers-price-helper', options);
+            Object.keys(output.results.fail).should.eql([
+                'GS090-NO-PRICE-DATA-CURRENCY-CONTEXT'
+            ]);
+
+            output.results.fail['GS090-NO-PRICE-DATA-CURRENCY-CONTEXT'].should.be.a.ValidFailObject();
+        });
+
+        it('should fail when {{@price.monthly}} is used', async function () {
+            const output = await utils.testCheck(thisCheck, '090-template-syntax/no-price-data-monthly-yearly/global', options);
+            Object.keys(output.results.fail).should.eql([
+                'GS090-NO-PRICE-DATA-MONTHLY-YEARLY'
+            ]);
+
+            output.results.fail['GS090-NO-PRICE-DATA-MONTHLY-YEARLY'].should.be.a.ValidFailObject();
+        });
+
+        it('should fail when {{price @price.monthly}} is used', async function () {
+            const output = await utils.testCheck(thisCheck, '090-template-syntax/no-price-data-monthly-yearly/price-helper', options);
+            Object.keys(output.results.fail).should.eql([
+                'GS090-NO-PRICE-DATA-MONTHLY-YEARLY'
+            ]);
+
+            output.results.fail['GS090-NO-PRICE-DATA-MONTHLY-YEARLY'].should.be.a.ValidFailObject();
         });
 
         it('should fail when {{monthly_price.*}} is used in tiers', async function () {
