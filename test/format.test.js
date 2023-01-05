b2a3e03ef3b9766b9f41a08aed3adc6d31f27eeb
Naz <hi@nazavo.com>
2023-01-05 17:54:07 +0700

    Relaxed {{error.code}} rule to non-fatal
    refs https://github.com/TryGhost/Toolbox/issues/497
    refs https://github.com/TryGhost/gscan/commit/c7a101be5b684fa4926efda6394c44bb26374551
    refs https://github.com/TryGhost/gscan/commit/e28d535117463b8ce9de027bb97bd912c1c0c09f
    
    - The classification of fatal/non-fatal errors has been updated to only throw fatal when causing page renders with 5xx or 4xx responses.
    - Using rule GS001-DEPR-ESC - {{error.code}}, {{code}} helpers, in Ghost 5.x causes empty output - no page render error. This behavior grants downgrade to regular, non-fatal error type.
    - The error is originally defined in the v3 rules set with error level. Removing the rule from canary rules set defaults the error to v3 rules set.

--- test/format.test.js
+++ test/format.test.js
@@ -418,7 +418,7 @@ describe('Format', function () {
                 const errorErrors = theme.results.error.all
                     .filter(error => (error.level === 'error') && !error.fatal);
 
-                errorErrors.length.should.eql(31);
+                errorErrors.length.should.eql(32);
                 errorErrors.map(e => e.code).should.eql([
                     'GS001-DEPR-MD',
                     'GS001-DEPR-PPP',
@@ -427,6 +427,7 @@ describe('Format', function () {
                     'GS001-DEPR-CSS-PATS',
                     'GS001-DEPR-AUTH',
                     'GS001-DEPR-BLOG',
+                    'GS001-DEPR-ESC',
                     'GS001-DEPR-LABS-MEMBERS',
                     'GS001-DEPR-CURR-SYM',
                     'GS001-DEPR-SITE-LANG',
@@ -454,7 +455,7 @@ describe('Format', function () {
                 ]);
 
                 const fatalErrors = theme.results.error.all.filter(error => error.fatal);
-                fatalErrors.length.should.eql(72);
+                fatalErrors.length.should.eql(71);
                 fatalErrors.map(e => e.code).should.eql([
                     'GS001-DEPR-PURL',
                     'GS001-DEPR-IMG',
@@ -522,7 +523,6 @@ describe('Format', function () {
                     'GS001-DEPR-SGH',
                     'GS001-DEPR-SGF',
                     'GS001-DEPR-LANG',
-                    'GS001-DEPR-ESC',
                     'GS005-TPL-ERR',
                     'GS090-NO-PRODUCTS-HELPER',
                     'GS090-NO-AUTHOR-HELPER-IN-POST-CONTEXT',
