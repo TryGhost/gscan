34b48bd1fbb3753712be50a5da793d33df07dc67
Filip Chabik <filip@ghost.org>
2025-06-20 15:36:37 +0200

    Made Sentry middleware compatible with version 9
    no issue
    
    - Sentry.Handlers.requestHandler() and Sentry.Handlers.errorHandler() are no longer available
    - Initializing and importing Sentry should be enough from now on

--- app/middlewares/sentry.js
+++ app/middlewares/sentry.js
@@ -1,9 +1,5 @@
 const sentryDSN = process.env.SENTRY_DSN;
 
-const expressNoop = function (req, res, next) {
-    next();
-};
-
 if (sentryDSN) {
     const Sentry = require('@sentry/node');
     const version = require('../../package.json').version;
@@ -12,14 +8,4 @@ if (sentryDSN) {
         release: 'gscan@' + version,
         environment: process.env.NODE_ENV
     });
-
-    module.exports = {
-        requestHandler: Sentry.Handlers.requestHandler(),
-        errorHandler: Sentry.Handlers.errorHandler()
-    };
-} else {
-    module.exports = {
-        requestHandler: expressNoop,
-        errorHandler: expressNoop
-    };
 }
